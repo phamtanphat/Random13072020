7c9c9ae8ab1659dce327c0d34703970d6d821234
phatdroid94 <phatdroid94@gmail.com>
2020-07-22 18:24:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/random13072020/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.random13072020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ctrl + p : xem tham so truyen vao
+        int value = tinhTong(5,10);
+        Log.d("BBB" ,String.valueOf(value));
+    }
+    // 1 : Pham vi truy cap : Access modifier (class , bien , function)
+    // 2 : Gia tri sau khi thuc thi
+    // 3 : Ten cua phuong thuc (theo camelcase)
+    // public , private , protected , default
+    private int tinhTong(int a , int b){
+        int ketqua = a + b;
+        return ketqua;
+    }
+}
