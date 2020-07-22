e0d9e22bf0fa708fbb3ae0a0c24cb23a7af1b33f
phatdroid94 <phatdroid94@gmail.com>
2020-07-22 19:08:05 +0700

    random

--- app/src/main/java/com/example/random13072020/MainActivity.java
+++ app/src/main/java/com/example/random13072020/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.random13072020;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
-import java.lang.annotation.Annotation;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,19 +12,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ctrl + p : xem tham so truyen vao
-        int value = tinhTong(5,10);
-        Log.d("BBB" ,String.valueOf(value));
+        // Lam tron : 0.5
+//        long value = Math.round(1.49);
+//        Log.d("BBB","Round value " + value);
+
+        // Lam tron len : 0.1
+//        double value1 = Math.ceil(0.1);
+//        Log.d("BBB","Ceil value " + value1);
+
+        // Lam tron xuong : 0.9
+//        double value2 = Math.floor(0.9);
+//        Log.d("BBB","Floor value " + value2);
+
+        // Ham random
+        // 0 - 5
+        double random = Math.floor(Math.random() * 6);
+        Log.d("BBB",random + "");
+
+
     }
-    // 1 : Pham vi truy cap : Access modifier (class , bien , function)
-    // 2 : Gia tri sau khi thuc thi
-    // 3 : Ten cua phuong thuc (theo camelcase)
-    // public , private , protected , default
-    private int tinhTong(int a , int b){
-        int ketqua = a + b;
-        return ketqua;
-    }
-    private void inThongBao(String msg){
-        Toast.makeText(MainActivity.this,msg,Toast.LENGTH_LONG).show();
-    }
+
+
+
+
+//    private void dinhNghiaPhuongThuc(){
+//        // ctrl + p : xem tham so truyen vao
+////        int value = tinhTong(5,10);
+////        Log.d("BBB" ,String.valueOf(value));
+//        inThongBao("Activity running");
+//        // 1 : Pham vi truy cap : Access modifier (class , bien , function)
+//        // 2 : Gia tri sau khi thuc thi
+//        // 3 : Ten cua phuong thuc (theo camelcase)
+//        // public , private , protected , default
+//        private int tinhTong(int a , int b){
+//            int ketqua = a + b;
+//            return ketqua;
+//        }
+//        private void inThongBao(String msg){
+//            Toast.makeText(MainActivity.this,msg,Toast.LENGTH_LONG).show();
+//        }
+//    }
 }
