e482caee0986515492aca8ab20d124fcd603c12d
phatdroid94 <phatdroid94@gmail.com>
2020-07-27 18:16:55 +0700

    method arraylist

--- app/src/main/java/com/example/random13072020/MainActivity.java
+++ app/src/main/java/com/example/random13072020/MainActivity.java
@@ -11,7 +11,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,39 +37,39 @@ public class MainActivity extends AppCompatActivity {
 
         mRandom = new Random();
 
-        mBtnRandom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Validate
-                String textSmin = mEdtSomin.getText().toString();
-                String textSmax = mEdtSomax.getText().toString();
-                if (textSmin.isEmpty() || textSmax.isEmpty()){
-                    Toast.makeText(
-                            MainActivity.this,
-                            "Bạn chưa nhập đủ thông tin",
-                            Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                // Điều kiện 1 : Số min không lớn hơn số max
-                // Điêu kiện 2 : Số max = 0 => min = 0 , max = 1
-
-                int sMin = Integer.parseInt(textSmin);
-                int sMax = Integer.parseInt(textSmax);
-
-                if (sMin >= sMax){
-                    sMax = sMin + 1;
-                }
-                // Hien thi
-
-                mRandomValue = mRandom.nextInt(sMax - sMin + 1) + sMin;
-                mValue = mValue + mRandomValue + " - ";
-
-                mTvKetqua.setText(mValue);
-                mEdtSomin.setText(sMin + "");
-                mEdtSomax.setText(sMax + "");
-
-            }
-        });
+//        mBtnRandom.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Validate
+//                String textSmin = mEdtSomin.getText().toString();
+//                String textSmax = mEdtSomax.getText().toString();
+//                if (textSmin.isEmpty() || textSmax.isEmpty()){
+//                    Toast.makeText(
+//                            MainActivity.this,
+//                            "Bạn chưa nhập đủ thông tin",
+//                            Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                // Điều kiện 1 : Số min không lớn hơn số max
+//                // Điêu kiện 2 : Số max = 0 => min = 0 , max = 1
+//
+//                int sMin = Integer.parseInt(textSmin);
+//                int sMax = Integer.parseInt(textSmax);
+//
+//                if (sMin >= sMax){
+//                    sMax = sMin + 1;
+//                }
+//                // Hien thi
+//
+//                mRandomValue = mRandom.nextInt(sMax - sMin + 1) + sMin;
+//                mValue = mValue + mRandomValue + " - ";
+//
+//                mTvKetqua.setText(mValue);
+//                mEdtSomin.setText(sMin + "");
+//                mEdtSomax.setText(sMax + "");
+//
+//            }
+//        });
 
 //        Yêu cầu
 //        1 : Nhập dữ liệu (validate)
@@ -77,6 +80,27 @@ public class MainActivity extends AppCompatActivity {
 //                + Các giá trị không được hiển thị quá 2 lần : 1 - 2 - 4 - 5
 //        ArrayList  trong collections
 //        Xu lý chuỗi split ,subString
+
+        ArrayList<String> arrayNames = new ArrayList<>();
+
+        // Thêm
+        arrayNames.add("Nguyen Van Teo");
+        arrayNames.add("Nguyen Van Ti");
+
+        // Cập nhật
+//        arrayNames.set(2 , "Nguyen Van Tuan");
+
+        // Xóa
+        arrayNames.remove(0);
+
+        Log.d("BBB",arrayNames.get(0) + "");
+
     }
+
+    // Task
+    // 1 : Hien thị giao diện
+//        - Button add range (Khi người dùng thêm giá trị random )
+//        - Button reset (Khi giá trị random đã tồn tại , khi hết số random)
+//        - Button random(Khi giá trị random lớn hơn 0)
 
 }
